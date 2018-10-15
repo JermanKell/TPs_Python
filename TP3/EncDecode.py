d8992b9af02b56486abe0a81097c1c0926e89084
JermanKell <xbouchenard@gmail.com>
2018-10-15 12:19:43 +0200

    fin tp3

--- TP3/EncDecode.py
+++ TP3/EncDecode.py
@@ -1,5 +1,5 @@
 import Cryptodome.Cipher.AES as AES
-from Cryptodome import Random
+from Cryptodome.Random import get_random_bytes
 
 
 class EncoDecode:
@@ -8,8 +8,11 @@ class EncoDecode:
 
     def setKey(self, key):
         self._Key=key
-        generatedValue = Random.new().read(AES.block_size)
-        self._aes = AES.new(self._Key, AES.MODE_CBC, generatedValue)
+        print(self._Key)
+        print(len(self._Key))
+        value = get_random_bytes(32)
+        print(value)
+        self._aes = AES.new(self._Key, AES.MODE_CBC, value)
 
     def Encode(self, dataToEncode):
         encMsg = self._aes.encrypt(dataToEncode)
